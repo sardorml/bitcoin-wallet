4f79df28cfadbb3f9f6eeef1214a5848faec485c
sardorml <sardormamadaliyev@gmail.com>
2024-02-04 00:01:45 +0900

    setup bitcoin libraries

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { mnemonic, generateWallet } from "./helpers/bitcoin";
+
+console.log(mnemonic);
+console.log(generateWallet(mnemonic));
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Bitcoin wallet</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
